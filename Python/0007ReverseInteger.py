7e38201da768023ae725872408bc267638319156
MarginCode <jerrywang015@gmail.com>
2022-02-19 00:09:07 +0800

    Update 0007ReverseInteger.py
    update 0007

--- Python/0007ReverseInteger.py
+++ Python/0007ReverseInteger.py
@@ -1,3 +1,12 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        return(0-x)
+        x=list(str(x))
+        result=[]
+        for i in range(len(x)-1,-1,-1):
+            result.append(x[i])
+        if result[-1]=='-':
+            result.pop()
+            result.insert(0,'-')
+        print(result)
+        
+        return int(''.join(result))
